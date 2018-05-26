1f069765156a53d5bc4992bb56cf864b4ed2c57b
The Code Worm <noel@thecodeworm.com>
2018-05-26 15:05:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ var T = new Twit({
 var users = [" ... ", " ... ", " ... "];
 
 var stream = T.stream('statuses/filter', {follow: users});
+
+stream.on('tweet', function (tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+            console.log(data)
+        })
+    }
+})
