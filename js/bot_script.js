05675c220dba2400c5d17a99ddfb68f7c58dd4a3
TheCodeWorm <4purposeliving@gmail.com>
2018-06-01 10:19:18 -0700

    commit to save

---
+++ js/bot_script.js
@@ -0,0 +1,17 @@
+
+console.log("testing.....");
+
+fetch('../credentials.json').then(function(res) {
+		console.log(res.json.text();  // convert to text
+})
+
+/*
+fetch('../credentials.json')
+	.then(console.log(response))
+	*/
+ /*
+   .then(response => response.json())
+  .then(jsonResponse => console.log(jsonResponse))
+  
+ */
+  
