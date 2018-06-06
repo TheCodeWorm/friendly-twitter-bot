16149bb3aa366241a89d1b3077b3a06e3cf1f18b
TheCodeWorm <4purposeliving@gmail.com>
2018-06-06 09:44:15 -0700

    commit to save

--- app.js
+++ app.js
@@ -1,111 +1,128 @@
 
-var Twitter = require('twitter');
-var config = require('./config.js');
+const Twitter = require('twitter');
+const config = require('./config.js');
 
-var T = new Twitter(config);
+let count_likes = 0;
+let count_follows = 0;
 
-// Set up your search parameters
-var params = {
-  q: '#100daysofcode',
-  count: 100,
-  result_type: 'recent',
-  lang: 'en'
+// command line arg passed 
+if (!process.argv[2]) {
+	addTo()
 }
 
-// Initiate your search using the above paramaters
-T.get('search/tweets', params, function(err, data, response) {
-  // If there is no error, proceed
-  if(!err){
-    // Loop through the returned tweets
-    for(let i = 0; i < data.statuses.length; i++){
-    	console.log("\nIteration "+i+1+": ")
+// clean out following list
+else if (process.argv[2] === "clean") {
+	let next_cursor = -1;
+	let getData = function(next_cursor) {
+		let T = new Twitter(config);
+	  T.get('friends/list', {
+	  screen_name: 'The_Code_Worm', cursor: next_cursor
+		}, (err, data, response) => {
+		  if (err) {
+		    console.log(err)
+		  } 
+		  else {
+		    //console.log("\n", data)
+		    for (let i = 0; i < data.users.length; i++) {
+			    console.log(data.users[i].screen_name);
+			    //console.log(data.user.following)
+		  	}
+		  	;
+				if(next_cursor != 0) { 
+					getData(next_cursor = data['next_cursor']);
+				}
+			}
+		});
+	}
+	getData(next_cursor);
+}
 
-    	// Get the tweet Id from the returned data
-    	let id = { id: data.statuses[i].id_str };
+function addTo() {
+	// array for searches
+	const searchArr = ['#100daysofcode'/*, '#javascript', '#nodejs', '#301daysofcode', '#30days30sites', '#codenewbie'*/];
 
-    	// if valid status
-      if (data.statuses[i].retweeted_status) { 
+	for (let i = 0; i < searchArr.length; i++) {
+		let T = new Twitter(config);
 
-      	let followers = data.statuses[i].retweeted_status.user.followers_count;
-      	let following = data.statuses[i].retweeted_status.user.friends_count;
-      	let followingBool = data.statuses[i].retweeted_status.user.following;
+		// Set up your search parameters
+		const params = {
+		  q: searchArr[i],
+		  count: 10,
+		  result_type: 'recent',
+		  lang: 'en'
+		}
 
-      	if (followingBool === false && following > followers) {
-	      	console.log("created: ", data.statuses[i].retweeted_status.created_at);
-	      	let screenName = data.statuses[i].retweeted_status.user.screen_name;
-	      	console.log("screen_name: ", screenName);
-	      	//console.log("description: ", data.statuses[i].retweeted_status.user.description);
-	      	console.log("friends_count: ", data.statuses[i].retweeted_status.user.friends_count);
-	      	console.log("followers_count: ", data.statuses[i].retweeted_status.user.followers_count);
-	      	console.log("following: ", data.statuses[i].retweeted_status.user.following);
-      		console.log("not follwing and they are following  more people than they have as followers");
+		// Initiate your search using the above paramaters
+		T.get('search/tweets', params, function(err, data, response) {
+		  // If there is no error, proceed
+		  if(!err){
+		    // Loop through the returned tweets
+		    for(let i = 0; i < data.statuses.length; i++){
+		    	console.log("\nIteration "+i+1+": ")
 
-      		
-      		console.log("Ready to create friendship:");
-      		T.post('friendships/create', {
-      			screen_name: screenName
-					}, (err, data, response) => {
-						if (err) {
-						  console.log("Error: ", err)
-						} else {
-						  //console.log(data)
-						  console.log("follow created!")
-						}
-					});
-					
+		    	// Get the tweet Id from the returned data
+		    	let id = { id: data.statuses[i].id_str };
 
-      		console.log("Ready to Like post:")
-					T.post('favorites/create', id, function(err, response) { 
-		        // If the favorite fails, log the error message
-		        if(err) {
-          		console.log(err[0].message);
-        	}
-	        // If the favorite is successful, log the url of the tweet
-	        else {
-	          let username = response.user.screen_name;
-	          let tweetId = response.id_str;
-	          console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
-	        }
-      });
+		    	// if valid status
+		      if (data.statuses[i].retweeted_status) { 
 
-      		/*
-					// follow user
-		      T.post('friendships/create', id, function(err, response) {
-		      	if (err) {
-		      		console.log(err[0].message);
-		      	}
-		      	else {
-		      		let username = response.user.screen_name;
-		          let tweetId = response.id_str;
-		          console.log('Followed: ', `https://twitter.com/${username}/status/${tweetId}`)
-		      	}
-		    	});
+		      	let followers = data.statuses[i].retweeted_status.user.followers_count;
+		      	let following = data.statuses[i].retweeted_status.user.friends_count;
+		      	let followingBool = data.statuses[i].retweeted_status.user.following;
 
-		      // like users post
-		    	T.post('favorites/create', id, function(err, response){
-		        // If the favorite fails, log the error message
-		        if(err){
-		          console.log(err[0].message);
-		        }
-		        // If the favorite is successful, log the url of the tweet
-		        else {
-		          let username = response.user.screen_name;
-		          let tweetId = response.id_str;
-		          console.log('Liked: ', `https://twitter.com/${username}/status/${tweetId}`)
-		        }
+		      	if (followingBool === false && following > followers) {
+			      	console.log("created: ", data.statuses[i].retweeted_status.created_at);
+			      	let screenName = data.statuses[i].retweeted_status.user.screen_name;
+			      	console.log("screen_name: ", screenName);
+			      	//console.log("description: ", data.statuses[i].retweeted_status.user.description);
+			      	console.log("friends_count: ", data.statuses[i].retweeted_status.user.friends_count);
+			      	console.log("followers_count: ", data.statuses[i].retweeted_status.user.followers_count);
+			      	console.log("following: ", data.statuses[i].retweeted_status.user.following);
+		      		console.log("not follwing and they are following  more people than they have as followers");
+		      		
+		      		console.log("Ready to create friendship:");
+		      		T.post('friendships/create', {
+		      			screen_name: screenName
+							}, (err, data, response) => {
+								if (err) {
+								  console.log("Error: ", err)
+								} else {
+								  //console.log(data)
+								  count_follows++;
+								  console.log(count_follows, "follows created!")
+								}
+							});
+
+		      		console.log("Ready to Like post:")
+							T.post('favorites/create', id, function(err, response) { 
+				        // If the favorite fails, log the error message
+				        if(err) {
+		          		console.log(err[0].message);
+		        	}
+			        // If the favorite is successful, log the url of the tweet
+			        else {
+			          let username = response.user.screen_name;
+			          let tweetId = response.id_str;
+			          count_likes++;
+			          console.log('Favorited: ', count_likes, `https://twitter.com/${username}/status/${tweetId}`)
+			        }
 		      });
-		      */
-      	}
-      	else { 
-    			console.log("did not fit requirements");
-    		}
-    	}
-    	else {
-    		console.log("There is no.. data.statuses[i].retweeted_status")
-    	}
-	  }
-  } 
-  else {
-    console.log(err);
-  }
-})
+
+		      	}
+		      	else { 
+		    			console.log("did not fit requirements");
+		    		}
+		    	}
+		    	else {
+		    		console.log("There is no.. data.statuses[i].retweeted_status")
+		    	}
+			  }
+		  } 
+		  else {
+		    console.log(err);
+		  }
+		});
+	}
+	
+}
+
